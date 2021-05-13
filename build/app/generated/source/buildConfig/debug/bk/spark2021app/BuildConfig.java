6ec3e61168a07045c271ab2be1005a7d12046b39
Somya1911 <khatrisomya18@gmail.com>
2021-05-14 02:16:49 +0530

    Key.properties added

--- build/app/generated/source/buildConfig/debug/bk/spark2021app/BuildConfig.java
+++ build/app/generated/source/buildConfig/debug/bk/spark2021app/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "bk.spark2021app";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 3;
-  public static final String VERSION_NAME = "1.2.1";
+  public static final int VERSION_CODE = 5;
+  public static final String VERSION_NAME = "2.0.1";
 }
